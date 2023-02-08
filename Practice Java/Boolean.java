b578107af6c70b08b564111980dc05a42843da7c
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-09 02:11:07 +0600

    Boolean

---
+++ Practice Java/Boolean.java
@@ -0,0 +1,9 @@
+public class Boolean {
+    public static void main(String[] args){
+        boolean x,y;
+        x = true;
+        y = false;
+        System.out.println("X is :" +x);
+        System.out.println("Y is :" +y);
+    }
+}
