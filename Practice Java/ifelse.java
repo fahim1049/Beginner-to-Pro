47b85092ee396631b5d64f190cd80f593517af2a
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-09 03:24:19 +0600

    if else condition

---
+++ Practice Java/ifelse.java
@@ -0,0 +1,13 @@
+public class ifelse {
+
+        public static void main(String[] args){
+            int sum;
+            sum = 100;
+            if(sum>200){
+                System.out.println("Fahim is bad boy");
+            }else{
+                System.out.println("Fahim is good boy");
+            }
+        }
+    }
+
