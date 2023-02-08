eee97adae504e08b244fc5f8cef27747f2972426
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-09 02:04:48 +0600

    Integer

---
+++ Practice Java/Int.java
@@ -0,0 +1,11 @@
+public class Int {
+    public static void main(String[] args){
+        int a,b,c;
+        a = 30;
+        b = -40;
+        c = 20;
+        float avg;
+        avg=(float)(a+b+c)/3;
+        System.out.println( "Avarage of abc :" +avg);
+    }
+}
