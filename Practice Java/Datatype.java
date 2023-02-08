66fe13ce04b506fb4da6639537cc98aebe511f19
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-09 01:53:31 +0600

    Variable declaration

---
+++ Practice Java/Datatype.java
@@ -0,0 +1,8 @@
+public class Datatype {
+    public static void main(String[] args){
+        int number = 100;
+        number = number+30;
+
+        System.out.println("My value is :" +number);
+    }
+}
