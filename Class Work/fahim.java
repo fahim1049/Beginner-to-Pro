d1cd79b43f4fa6d9a97658ed60aff6735881d201
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-10 23:28:45 +0600

    Print name,age,etc

---
+++ Class Work/fahim.java
@@ -0,0 +1,8 @@
+public class fahim {
+   public static void main(String[] args){
+        System.out.println("MD.Nurnabi Fahim");
+        System.out.println("Nickname: Fahim");
+        System.out.println("Age: 22");
+        System.out.println("Address:DIU");
+    }
+}
