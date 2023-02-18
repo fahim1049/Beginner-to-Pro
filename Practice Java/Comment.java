4e322fb193e3263b5841ee4086032d27912aaaab
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-18 15:57:49 +0600

    Add files via upload

---
+++ Practice Java/Comment.java
@@ -0,0 +1,11 @@
+/*
+ That is Comment program
+ with single line and multiple line
+ by fahim
+ */
+public class Comment {
+    public static void main(String[] args){
+        //Printing my name with single line comment
+        System.out.println("Fahim");
+    }
+}
