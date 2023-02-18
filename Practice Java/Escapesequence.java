e6c6859b08bd5b0094b6a5457811d655142faefc
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-18 15:43:15 +0600

    Escape Sequence

---
+++ Practice Java/Escapesequence.java
@@ -0,0 +1,8 @@
+public class Escapesequence {
+    public static void main(String[] args){
+        System.out.println("Fahim is a good Boy\nStudent Of Daffodil International University\nDepartment of SWE");
+        System.out.println("1\t2");
+        System.out.println("3\t4");
+        System.out.println("\"DIU_SWE_37TH\"");
+    }
+}
