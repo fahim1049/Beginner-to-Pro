549d342f40abe5a431fee7413bb75cd7678c260f
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-19 21:18:56 +0600

    Data Type

---
+++ Practice Java/DataType.java
@@ -0,0 +1,24 @@
+public class DataType {
+    public static void main(String[] args){
+        boolean b;
+        float f;
+        int n;
+        double d;
+        short s;
+        char c;
+        b = true;
+        f = 12.5f;
+        n = 1200;
+        d = 12.45;
+        s = 12345;
+        c = 'f';
+
+        System.out.println("b :" +b);
+        System.out.println("f :" +f);
+        System.out.println("n :" +n);
+        System.out.println("d :" +d);
+        System.out.println("s :" +s);
+        System.out.println("c :" +c);
+
+    }
+}
