35ea808d7ae85447a7cb2af11f4411744c022dd9
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-02-22 22:22:40 +0600

    Switch Case

---
+++ Class Work/SwitchCase.java
@@ -0,0 +1,26 @@
+public class SwitchCase {
+    public static void main(String[] args){
+        int x = 6;
+        switch(x){
+            case 1:
+                System.out.println("case 1");
+                break;
+                case 2:
+                    System.out.println("case 2");
+                    break;
+                case 3:
+                    System.out.println("case 3");
+                    break;
+            case 4:
+                System.out.println("case 4");
+                break;
+            case 5:
+                System.out.println("case 5");
+                break;
+            default:
+                System.out.println("Error");
+        }
+
+
+    }
+}
