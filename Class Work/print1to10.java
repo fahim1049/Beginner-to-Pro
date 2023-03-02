8609e9dc59ef5c7626ae85b1ca4d56bad0229fe5
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-03-02 19:38:43 +0600

    Print 1 to 10

---
+++ Class Work/print1to10.java
@@ -0,0 +1,15 @@
+public class print1to10 {
+    public static void main(String args[])
+    {
+
+        int i=1;
+
+        System.out.println("Output is : ");
+
+        while(i<=10)
+        {
+            System.out.println(i);
+            i++;
+        }
+    }
+}
