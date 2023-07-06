796c28b5a4c6dfd20a6e2a3fc1d2e984dcac6ed9
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-07-06 15:02:07 +0600

    User Input

---
+++ Practice Java/Code with Anisul islam Sir/fahim.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class fahim {
+    public static void main(String[]args){
+        Scanner input = new Scanner (System.in);
+        int number;
+        System.out.println("Enter number :");
+        number = input.nextInt();
+        System.out.println("Number is : "+number);
+
+    }
+}
