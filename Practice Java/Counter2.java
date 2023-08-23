f91e875cc40ab4524584c738e1a3dca9fd017e02
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-08-23 23:01:54 +0600

    Print 0-10 with counter

---
+++ Practice Java/Counter2.java
@@ -0,0 +1,8 @@
+public class Counter2 {
+    public static void main(String[]args){
+        //counter++ => counter = counter + 1
+        for(int counter = 0; counter < 11; counter++){
+            System.out.println(counter);
+        }
+    }
+}
