bf95da5143eb498d6a434be7d5f7810ccafdb009
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-08-23 23:02:57 +0600

    Print 0-10 single line with counter

---
+++ Practice Java/counter3.java
@@ -0,0 +1,8 @@
+public class counter3 {
+    public static void main(String[]args){
+        for(int counter = 0; counter < 10; counter++){
+            System.out.print(counter+" ");
+        }
+    }
+
+}
