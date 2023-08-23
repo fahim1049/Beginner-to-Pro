905baba9c19f62e0bb96943ac58be757c9376772
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-08-23 23:00:43 +0600

    Print 1-100 by counter

---
+++ Practice Java/Counter.java
@@ -0,0 +1,8 @@
+public class Counter {
+    public static void main(String[]args){
+        for(int counter = 0;counter < 100; counter = counter + 1){
+
+            System.out.println("Hello World");
+        }
+    }
+}
