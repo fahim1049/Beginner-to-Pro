92d85a041c3c3df18c619bcbefb73b5ddf6b73d7
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-08-23 23:03:50 +0600

    While in java

---
+++ Practice Java/Whilw.java
@@ -0,0 +1,9 @@
+public class Whilw {
+    public static void main(String[]args){
+        int i = 0;
+        while(i < 11){
+            System.out.println(i);
+            i = i + 1; //i++
+        }
+    }
+}
