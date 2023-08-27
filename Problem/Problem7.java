b3be607f873fd3cc86a9bafa25d86af5bb704736
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2023-08-27 12:58:21 +0600

    Print the pattern

---
+++ Problem/Problem7.java
@@ -0,0 +1,23 @@
+
+//Print the pattern
+               //1
+               //2 3
+              // 4 5 6
+               //7 8 9 10
+               //11 12 13 14 15
+
+
+
+public class Problem7 {
+   public static void main(String[]args){
+       int n = 5;
+       int number = 1;
+       for(int i=1; i<=n; i++){
+           for(int j=1; j<=i; j++){
+               System.out.print(number+" ");
+               number++;
+           }
+           System.out.println();
+       }
+    }
+}
