fbddf68b5db7293c445c4d3a6be08b4f88100790
Nurnabi Fahim <98411093+fahim1049@users.noreply.github.com>
2024-01-17 09:55:49 +0600

    Add files via upload

---
+++ Practice Java/Code with Anisul islam Sir/Assignment1.java
@@ -0,0 +1,18 @@
+
+
+package com.mycompany.assignment1;
+
+/**
+ *
+ * @author fahim
+ */
+public class Assignment1 {
+
+    public static void main(String[]args){
+        System.out.println("id: 101,");
+        System.out.println("title :iphone15,");
+        System.out.println("price : 1895 euros,");
+        System.out.println("decription: perfect product with best image quality,");
+        System.out.println("category: phone,");
+    }
+}
